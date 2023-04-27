d1b927501a5dd9406d6a14978b5a3577050fb08b
rishita-97 <124322832+rishita-97@users.noreply.github.com>
2023-04-26 18:07:38 -0700

    valid Anagram

---
+++ Arrays and Hashing/242. Valid Anagram/validAnagram.java
@@ -0,0 +1,18 @@
+class Solution{
+public boolean isAnagram(String s, String t) {
+    if (s.length() != t.length()) {
+        return false;
+    }
+    int[] alphabet = new int[26];
+    for (int i = 0; i < s.length(); i++) {
+        alphabet[s.charAt(i) - 'a']++;
+        alphabet[t.charAt(i) - 'a']--;
+    }
+    for (int count : alphabet) {
+        if (count != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+}
